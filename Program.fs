ecb46d9d09153cbd63acdebe88c4790ee0794cae
Simon Timms <stimms@gmail.com>
2016-12-30 22:10:30 -0700

    initial add

---
+++ Program.fs
@@ -0,0 +1,28 @@
+﻿open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open System.Net
+
+
+let home = OK "Hello home"
+
+[<EntryPoint>]
+let main argv = 
+    let app = 
+        choose 
+            [ GET >=> choose
+                [ path "/home" >=> home
+                  path "/about" >=> OK "All about" ]
+              POST >=> choose
+                [
+                    path "/home" >=> OK "Hello post to home"
+                    path "/about" >=> OK "Hello post to about"
+                ]
+            ]
+    let config = { defaultConfig with 
+                    bindings =
+                                [ HttpBinding.mk HTTP IPAddress.Loopback 9000us] }
+    startWebServer config app
+    0 // return an integer exit code
+
